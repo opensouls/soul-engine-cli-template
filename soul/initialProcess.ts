fc052783409e9cc92f073be272539508f18c7423
Kevin Fischer <fischer.kevin.a@gmail.com>
2024-02-05 12:41:54 -0800

    do a small simple template update (#3)
    - simplify learning about the user
    - add a secondary mental process if you attack the soul

--- soul/initialProcess.ts
+++ soul/initialProcess.ts
@@ -1,9 +1,10 @@
-
-import { externalDialog } from "socialagi";
-import { MentalProcess, useActions } from "soul-engine";
+import { externalDialog, mentalQuery } from "socialagi";
+import { MentalProcess, useActions, useProcessManager } from "soul-engine";
+import shouts from "./mentalProcesses/shouts.js";
 
 const gainsTrustWithTheUser: MentalProcess = async ({ step: initialStep }) => {
-  const { speak  } = useActions()
+  const { speak, log } = useActions()
+  const { setNextProcess } = useProcessManager()
 
   const { stream, nextStep } = await initialStep.next(
     externalDialog("Talk to the user trying to gain trust and learn about their inner world."),
@@ -11,7 +12,16 @@ const gainsTrustWithTheUser: MentalProcess = async ({ step: initialStep }) => {
   );
   speak(stream);
 
-  return nextStep
+  const lastStep = await nextStep
+  const shouldShout = await lastStep.compute(
+    mentalQuery("The interlocuter is being rude")
+  )
+  log("User attacked soul?", shouldShout)
+  if (shouldShout) {
+    setNextProcess(shouts)
+  }
+
+  return lastStep
 }
 
 export default gainsTrustWithTheUser
